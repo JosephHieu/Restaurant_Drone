6295b61d02470fadf25ddffc355ca53850299b1e
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-10-20 09:55:07 +0700

    delete rebundant folder

--- 02-frontend/restaurant-drone-delivery-old/src/layouts/HomePage/PromoBar.tsx
+++
@@ -1,9 +0,0 @@
-export default function PromoBar() {
-  return (
-    <div className="w-full bg-red-500 text-white py-3 px-4 text-center">
-      <p className="text-sm font-medium">
-        🎉 Giảm 30% cho đơn hàng đầu tiên - Miễn phí giao hàng cho đơn từ 100K
-      </p>
-    </div>
-  );
-}
